ce287e607ad97707fdf8ff00a967be690825c7d0
Maika Kailani <queenmab051@gmail.com>
2019-12-09 00:00:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,5 +50,5 @@ class Song
   def self.create_from_filename(name)
     song = self.new
     song.name = (name.split("-")[1].chomp(".mp3"))
-    song.artist_name = (name.split("-"))
+    song.artist_name = (name.split("-")[0])
 end
