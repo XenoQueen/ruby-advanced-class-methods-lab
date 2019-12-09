c00383c6af76b3d91bed896154f7ec7e886f4790
Maika Kailani <queenmab051@gmail.com>
2019-12-09 00:07:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,13 +23,13 @@ class Song
     song
   end
 
-  def self.create_by_name(title) #class constructor
+  def self.create_by_name(title)
     song = self.create
     song.name = title
     song
   end
 
-  def self.find_by_name(title) #class finder
+  def self.find_by_name(title)
     result = self.all.detect {|song| song.name == title}
     result
   end
